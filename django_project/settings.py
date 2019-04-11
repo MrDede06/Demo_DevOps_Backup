bb322363f63d7cdd7df6a1a7752d8196c5d07ab1
Ozgur Dede <ozgurdede006@gmail.com>
2019-04-11 22:47:06 +0200

    ready to deploy

--- django_project/settings.py
+++ django_project/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'main.apps.MainConfig',
     'tinymce',
+    'django_nose',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -149,3 +150,5 @@ TINYMCE_DEFAULT_CONFIG = {
     'menubar': True,
     'statusbar': True,
     }
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+
--- main/tests/test_SystemTests.py
+++ main/tests/test_SystemTests.py
@@ -1,9 +1,8 @@
-
 from selenium import webdriver
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from django.urls import reverse
 import time
-
+from main.views import *
 
 
 class MySystemTests(StaticLiveServerTestCase):
@@ -18,12 +17,25 @@ class MySystemTests(StaticLiveServerTestCase):
         self.options.add_argument("--no-sandbox")
         self.options.add_argument("--disable-dev-shm-usage")
         self.browser = webdriver.Chrome( options=self.options)        
-
+        self.data = {
+            'username': 'ttttestclientt',
+            'password1': 'test123',
+            'password2': 'test123',
+        }   
 
     def tearDown(self):
         self.browser.close()
 
+
     def test_user_register_and_check_tutorials(self):
         self.browser.get(self.live_server_url)
-        time.sleep(5)
- #       self.browser.close()
+        add_url = self.live_server_url + '/'
+        self.browser.find_element_by_tag_name('h1').click()
+        self.assertEquals(self.browser.current_url, add_url)
+
+    def test_user_login_and_check_tutorials(self):
+        self.browser.get(self.live_server_url)
+        add_url = self.live_server_url + '/#'
+        self.browser.find_element_by_tag_name('a').click()
+        self.assertEquals(self.browser.current_url, add_url)
+
